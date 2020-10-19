f98a0b4204d326d68c22f1682e12bfe0b8515474
Alok Rawat <alokrawat050@gmail.com>
2020-10-19 22:26:19 +0900

    added info in readme file.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # todo-using-flask
+
+Hi, you can check the live application on Heroku.
+https://todo-using-flask.herokuapp.com/
+UserId: user
+PWD: testuser
--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def welcome():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if (request.form['username'] != 'user' or request.form['password'] != 'testuser'):
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
