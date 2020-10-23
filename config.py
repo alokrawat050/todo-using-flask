6de43cb507885cdcb565e36b9c381210ebf44ea8
Alok Rawat <alokrawat050@gmail.com>
2020-10-23 23:13:31 +0900

    added migrations

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = 'Xdwewqesd123131!CFVFr453453g11'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+    # print(SQLALCHEMY_DATABASE_URI)
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
